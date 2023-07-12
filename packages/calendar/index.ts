bb206cd2372b549e9d7a7bdb0f06d3b5c2f3c224
staaph <contact.staaph@gmail.com>
2023-07-12 18:16:50 +0200

    feat: improve types

--- packages/calendar/index.ts
+++ packages/calendar/index.ts
@@ -1,5 +1,5 @@
 import { useCalendar } from "./src/useCalendar";
-import type { CalendarDate, Current } from "./types/calendar/CalendarTypes";
+import type { CalendarDate, Current } from "./types/CalendarTypes";
 import type { Dayjs } from "dayjs";
 
 export { useCalendar, CalendarDate, Current, Dayjs };
