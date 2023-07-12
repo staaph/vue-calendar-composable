a47a052ab1a0b8d9b62f24533113115da537cee8
staaph <contact.staaph@gmail.com>
2023-07-12 18:45:01 +0200

    refactor: remove unused types

--- packages/calendar/types/FormatOptions.ts
+++
@@ -1,6 +0,0 @@
-import { basicFormatOptions, localizedformatOptions } from "../src/constants";
-
-type BasicFormat = typeof basicFormatOptions[number]['format'];
-type LocalizedFormat = typeof localizedformatOptions[number]['format'];
-
-export type DayjsFormatOptions = BasicFormat | LocalizedFormat ;
